74dbb724975794e274bfed61a7b9a01ff2c76146
niklaswendel <niklas.wendel@gmail.se>
2022-12-31 11:05:19 +0100

    Enable implicit usings

--- src/FluentRegistration/Internal/Registration.cs
+++ src/FluentRegistration/Internal/Registration.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using FluentRegistration.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
